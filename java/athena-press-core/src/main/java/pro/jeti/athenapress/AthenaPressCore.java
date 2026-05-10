368490a39ca3943a9ec362c9d893204d770854cf
jeti83 <jeti83@hotmail.de>
2026-05-10 21:35:18 +0200

    Freeze AthenaPress backend v0.3 &start Java core

---
+++ java/athena-press-core/src/main/java/pro/jeti/athenapress/AthenaPressCore.java
@@ -0,0 +1,12 @@
+package pro.jeti.athenapress;
+
+public class AthenaPressCore {
+
+    public String getName() {
+        return "AthenaPress Core";
+    }
+
+    public String getVersion() {
+        return "0.1.0-SNAPSHOT";
+    }
+}
